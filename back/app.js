9fd173fadda5d8b8fda638ac1ee4f1ef86a8473d
acecio <adam_ceciora@hotmail.com>
2020-06-24 12:35:09 +0200

    removed a console.log and comments

--- back/app.js
+++ back/app.js
@@ -49,9 +49,7 @@ app.use(expressSession(
 // app.use(cors());
 
 app.use((req, res, next) => // Allows access to our API from any origin, with any method and any header.
-{  if (req.params.lang)
-    console.log('ici');
-    
+{
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Credentials', true); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
